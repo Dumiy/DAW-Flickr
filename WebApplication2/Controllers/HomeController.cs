4af8d3f5dbb35dd94800b59d792add31df118c41
Dumiy <dumitrascu.claudiu.cristian@gmail.com>
2020-01-06 15:22:58 +0200

    added upload images

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNet.Identity;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity.Validation;
 
 namespace WebApplication2.Controllers
 {
@@ -11,6 +13,33 @@ namespace WebApplication2.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        [Authorize(Roles = "User - Registred,Administrator")]
+        public ActionResult AddImage()
+        {
+            image Imagine = new image();
+            return View(Imagine);
+        }
+        [HttpPost]
+        [Authorize(Roles = "User - Registred,Administrator")]
+        public ActionResult AddImage(image model,HttpPostedFileBase image1)
+        {
+                var db = new Entities();
+                Console.Write(User.Identity.GetUserId().ToString());
+                if (image1 != null && User.Identity.IsAuthenticated)
+                {
+                    model.picture = new byte[image1.ContentLength];
+                    image1.InputStream.Read(model.picture, 0, image1.ContentLength);
+                    model.userId = User.Identity.GetUserId().ToString();
+                }
+                else
+                {
+                    Response.Redirect(Request.RawUrl);
+                }
+                db.images.Add(model);
+                db.SaveChanges();
+                return View(model);
+      
         }
 
         public ActionResult About()
