a663cf24666a1b2dcd84351911e2d478f1ab80e5
Razvan Cazacu <cmr.razvan@gmail.com>
2020-01-06 20:28:06 +0200

    Small Model Changes

--- WebApplication2/Models/PhotoFlickr.cs
+++ WebApplication2/Models/PhotoFlickr.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 using System.Drawing;
 using System.Linq;
 using System.Web;
@@ -18,6 +19,12 @@ namespace WebApplication2.Models
         [StringLength(6, ErrorMessage = "Titlul nu poate avea mai mult de 6 caractere")]
         public string Title { get; set; }
 
+        [Required(ErrorMessage = "Categoria este obligatorie")]
+        public int CategoryId { get; set; }
+
+        [Required(ErrorMessage = "Userul este obligatoriu")]
+        public string UserId { get; set; }
+
         [Required(ErrorMessage = "Continutul pozei este obligatoriu")]
         public Image Photo { get; set; }
 
@@ -27,17 +34,20 @@ namespace WebApplication2.Models
         [DataType(DataType.DateTime, ErrorMessage = "Campul trebuie sa contina data si ora")]
         public DateTime Date { get; set; }
 
-        [Required(ErrorMessage = "Categoria este obligatorie")]
-        public int CategoryId { get; set; }
-
-        public string UserId { get; set; }
-
         public virtual Comment Comment { get; set; }
-
-        public IEnumerable<SelectListItem> Comments { get; set; }
 
         public virtual ApplicationUser User { get; set; }
 
- 
+
+
+        public virtual ICollection<Comment> Comments { get; set; }
+    }
+    public class PhotoFlickrDBContext : DbContext
+    {
+        public PhotoFlickrDBContext() : base("DefaultConnection") { }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Albums> Albums { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
     }
 }
